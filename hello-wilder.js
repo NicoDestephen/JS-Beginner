2ddc93b3bee7cf4b1d7de1fe32ba160bc3f4660e
NicoDestephen <ndestephen@gmail.com>
2024-02-14 18:10:48 +0100

    ended js first quest

---
+++ hello-wilder.js
@@ -0,0 +1,10 @@
+/* hello-wilder.js */
+
+// Define some variables
+const filmName = 'Forest Gump';
+const realisator = 'Robert Zemeckis';
+const date = '1994';
+const message = filmName + ', réalisé par ' + realisator + ', est sorti en ' + date;
+
+// Display a popup
+alert(message);
